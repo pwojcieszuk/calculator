79fec8dd1ee084925a1192f928eeebe676afeb6b
Piotr Wojcieszuk <pwojcieszuk@gmail.com>
2023-12-14 18:40:18 +0100

    Add basic styles

--- src/components/Calculator.tsx
+++ src/components/Calculator.tsx
@@ -40,17 +40,25 @@ const Calculator = () => {
   };
 
   return (
-    <div>
-      <h1>Calculator</h1>
-      <div>
+    <div className="max-w-xs mx-auto mt-10 p-5 border-2 border-gray-300 rounded-lg shadow-lg">
+      <h1 className="text-2xl font-bold text-center text-gray-700 mb-4">Calculator</h1>
+      <div className="grid grid-cols-4 gap-2 mb-4">
         {buttons.map((button, idx) => (
-          <button key={idx} onClick={() => handleButtonClick(button)}>
+          <button
+            key={idx}
+            className="bg-blue-500 text-white font-semibold py-2 rounded hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-300"
+            onClick={() => handleButtonClick(button)}>
             {button}
           </button>
         ))}
       </div>
-      <input type="text" value={input} onChange={(e) => setInput(e.target.value)} />
-      {error && <p style={{ color: "red" }}>{error}</p>}
+      <input
+        type="text"
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+        className="w-full p-2 border-2 border-gray-300 rounded mb-2"
+      />
+      {error && <p className="text-red-500 text-sm">{error}</p>}
     </div>
   );
 };
